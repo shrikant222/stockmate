d43f937132873e37c753b26452c93fa10c6245d4
Shrikant chavan <shrikantchavan9404@gmail.com>
2026-03-27 23:15:13 +0530

    init commit

---
+++ src/main/java/com/stockmate/StockmateApplication.java
@@ -0,0 +1,13 @@
+package com.stockmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockmateApplication.class, args);
+    }
+
+}
